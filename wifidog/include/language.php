b8f2a05b0ccdfe9ed53fd0b013e34f268d64395c
Philippe April <philippe@ilesansfil.org>
2005-01-12 16:49:46 +0000

    *** empty log message ***

--- wifidog/include/language.php
+++ wifidog/include/language.php
@@ -5,6 +5,7 @@ if (!empty($_REQUEST['lang']) && isset($session)) {
 
 if (isset($session) && $session->get(SESS_LANGUAGE_VAR)) {
     putenv("LC_ALL=".$session->get(SESS_LANGUAGE_VAR));
+    putenv("LANGUAGE=".$session->get(SESS_LANGUAGE_VAR));
     setlocale(LC_ALL, $session->get(SESS_LANGUAGE_VAR));
     if (isset($smarty)) {
         $smarty->assign("lang_id", $session->get(SESS_LANGUAGE_VAR));
