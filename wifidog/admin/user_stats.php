d8fbdb42e8f62ba34c33c50fe34763489b92910e
Philippe April <philippe@ilesansfil.org>
2005-01-28 23:32:23 +0000

    *** empty log message ***

--- wifidog/admin/user_stats.php
+++ wifidog/admin/user_stats.php
@@ -24,34 +24,14 @@
    */
 define('BASEPATH','../');
 require_once 'admin_common.php';
+require_once BASEPATH.'Statistics.php';
 
 $smarty->assign("total_users",  $stats->getNumUsers());
 $smarty->assign("total_valid",  $stats->getNumValidUsers());
 
-$results = null;
-$db->ExecSql("SELECT COUNT(users) AS num_users, date_trunc('month', reg_date) AS month FROM users  WHERE account_status = ".ACCOUNT_STATUS_ALLOWED." GROUP BY date_trunc('month', reg_date) ORDER BY month DESC",$results, false);
-if ($results != null) {
-    $smarty->assign("registrations", $results);
-}
-
-
-$results = null;
-$db->ExecSql("SELECT COUNT(DISTINCT node_id) AS num_hotspots_visited, user_id FROM users NATURAL JOIN connections WHERE (incoming!=0 OR outgoing!=0) GROUP BY user_id ORDER BY num_hotspots_visited DESC LIMIT 10", $results, false);
-if ($results != null) {
-    $smarty->assign("most_mobile_users", $results);
-}
-
-$results = null;
-$db->ExecSql("SELECT COUNT(user_id) AS active_days, user_id FROM (SELECT DISTINCT user_id, date_trunc('day', timestamp_in) AS date FROM connections WHERE (incoming!=0 OR outgoing!=0) GROUP BY date,user_id) as user_active_days GROUP BY user_id ORDER BY active_days DESC LIMIT 10",$results, false);
-if ($results != null) {
-    $smarty->assign("most_frequent_users", $results);
-}
-
-$results = null;
-$db->ExecSql("SELECT DISTINCT user_id, SUM((incoming+outgoing)/1048576) AS total, SUM((incoming/1048576)) AS total_incoming, SUM((outgoing/1048576)) AS total_outgoing FROM connections GROUP BY user_id ORDER BY total DESC limit 10",$results, false);
-if ($results!=null) {
-    $smarty->assign("most_apetite_users", $results);
-}
-
+$smarty->assign("registrations", Statistics::getRegistrationsPerMonth());
+$smarty->assign("most_mobile_users", Statistics::getMostMobileUsers(10));
+$smarty->assign("most_frequent_users", Statistics::getMostFrequentUsers(10));
+$smarty->assign("most_greedy_users", Statistics::getMostGreedyUsers(10));
 $smarty->display("admin/templates/user_stats.html");
 ?>
--- wifidog/classes/Statistics.php
+++ wifidog/classes/Statistics.php
@@ -89,6 +89,30 @@ class Statistics{
     $db->ExecSqlUniqueRes("SELECT timestamp_in FROM connections WHERE incoming!=0 $sql_node ORDER BY timestamp_in DESC LIMIT 1", $row, false);
     return $row['timestamp_in'];
   }
+
+  public static function getRegistrationsPerMonth() {
+    global $db;
+    $db->ExecSql("SELECT COUNT(users) AS num_users, date_trunc('month', reg_date) AS month FROM users  WHERE account_status = ".ACCOUNT_STATUS_ALLOWED." GROUP BY date_trunc('month', reg_date) ORDER BY month DESC",$results, false);
+    return $results;
+  }
+
+  public static function getMostMobileUsers($limit) {
+    global $db;
+    $db->ExecSql("SELECT COUNT(DISTINCT node_id) AS num_hotspots_visited, user_id FROM users NATURAL JOIN connections WHERE (incoming!=0 OR outgoing!=0) GROUP BY user_id ORDER BY num_hotspots_visited DESC LIMIT $limit", $results, false);
+    return $results;
+  }
+
+  public static function getMostFrequentUsers($limit) {
+    global $db;
+    $db->ExecSql("SELECT COUNT(user_id) AS active_days, user_id FROM (SELECT DISTINCT user_id, date_trunc('day', timestamp_in) AS date FROM connections WHERE (incoming!=0 OR outgoing!=0) GROUP BY date,user_id) as user_active_days GROUP BY user_id ORDER BY active_days DESC LIMIT $limit",$results, false);
+    return $results;
+  }
+
+  public static function getMostGreedyUsers($limit) {
+    global $db;
+    $db->ExecSql("SELECT DISTINCT user_id, SUM((incoming+outgoing)/1048576) AS total, SUM((incoming/1048576)) AS total_incoming, SUM((outgoing/1048576)) AS total_outgoing FROM connections GROUP BY user_id ORDER BY total DESC limit $limit", $results, false);
+    return $results;
+  }
   
 }//End class
 ?>
