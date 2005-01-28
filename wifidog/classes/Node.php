8c5f87b9b724e9777805b640ab990fcf28f6d731
Philippe April <philippe@ilesansfil.org>
2005-01-28 23:54:38 +0000

    *** empty log message ***

--- wifidog/classes/Node.php
+++ wifidog/classes/Node.php
@@ -250,6 +250,10 @@ class Node
     return $row;
   }
 
+  function getInfoArray() {
+    return $this->mRow;
+  }
+
   public static function getAllOnlineUsers() {
     global $db;
     $db->ExecSql("SELECT * FROM connections,users,nodes WHERE token_status='" . TOKEN_INUSE . "' AND users.user_id=connections.user_id AND nodes.node_id=connections.node_id ORDER BY timestamp_in DESC", $online_users);
