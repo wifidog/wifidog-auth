8a97aaff4717075d870a1c8baa16c9ac0bd78cfe
Philippe April <philippe@ilesansfil.org>
2005-02-13 18:56:06 +0000

    *** empty log message ***

--- wifidog/locale/gen.sh
+++ wifidog/locale/gen.sh
@@ -5,9 +5,9 @@ for i in fr; do
     find ../admin/templates -name "*.html" -exec ./gensmarty.pl {} >> smarty.txt \;
 
     FILE="$i/LC_MESSAGES/messages.po"
-    find .. -maxdepth 1 -name "*.php" -exec xgettext -C -j -o $FILE --keyword=_ {} \;
+    find .. -maxdepth 1 -name "*.php" -exec xgettext --language=PHP --from-code=iso-8859-1 -j -o $FILE --keyword=_ {} \;
     for dir in admin classes include local_content login portal user_management; do
-        find ../$dir -maxdepth 1 -name "*.php" -exec xgettext -C -j -o $FILE --keyword=_ {} \;
+        find ../$dir -maxdepth 1 -name "*.php" -exec xgettext --language=PHP --from-code=iso-8859-1 -j -o $FILE --keyword=_ {} \;
     done
-    xgettext -C -j -o $FILE --keyword=_ smarty.txt
+    xgettext  --language=PHP --from-code=iso-8859-1 -j -o $FILE --keyword=_ smarty.txt
 done
