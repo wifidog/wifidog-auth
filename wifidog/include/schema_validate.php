bfc249049f6cc57f164048774f393136bab80359
François Proulx <francois.proulx@gmail.com>
2005-03-31 16:49:23 +0000

    2005-03-31 Francois Proulx  <francois.proulx@gmail.com> 	* Added missing schema_validate.php modifs.

--- wifidog/include/schema_validate.php
+++ wifidog/include/schema_validate.php
@@ -1,31 +1,33 @@
 <?php
-  /********************************************************************\
-   * This program is free software; you can redistribute it and/or    *
-   * modify it under the terms of the GNU General Public License as   *
-   * published by the Free Software Foundation; either version 2 of   *
-   * the License, or (at your option) any later version.              *
-   *                                                                  *
-   * This program is distributed in the hope that it will be useful,  *
-   * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
-   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
-   * GNU General Public License for more details.                     *
-   *                                                                  *
-   * You should have received a copy of the GNU General Public License*
-   * along with this program; if not, contact:                        *
-   *                                                                  *
-   * Free Software Foundation           Voice:  +1-617-542-5942       *
-   * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
-   * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
-   *                                                                  *
-   \********************************************************************/
-  /**@file schema_validate.php
-   * Network status page
-   * @author Copyright (C) 2004 Benoit Grégoire
-   */error_reporting(E_ALL);
+
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+ \********************************************************************/
+/**@file schema_validate.php
+ * Network status page
+ * @author Copyright (C) 2004 Benoit Grï¿½goire
+ */
+error_reporting(E_ALL);
 require_once BASEPATH.'config.php';
 require_once BASEPATH.'classes/AbstractDb.php';
 require_once BASEPATH.'classes/Session.php';
-define('REQUIRED_SCHEMA_VERSION', 2);
+define('REQUIRED_SCHEMA_VERSION', 3);
 
 /** Check that the database schema is up to date.  If it isn't, offer to update it. */
 function validate_schema()
@@ -64,45 +66,45 @@ function validate_schema()
 }
 
 /** Try to bring the database schema up to date. */
-function update_schema()
-{
-  global $db;
-  echo "<html><head><h1>\n";
-  echo _("Trying to update the database schema.");
-  echo "</h1>\n";
-  $db -> ExecSqlUniqueRes ("SELECT * FROM schema_info WHERE tag='schema_version'", $row, false);
-  
-  if(empty($row))
-    {
-      echo "<h1>"._("Unable to retrieve schema version.  The database schema is too old to be updated.")."</h1>";
-      exit();
-    }
-  else
-    {
-      $schema_version = $row['value'];
-      $sql='';
-      if($schema_version<2)
-	{
-	  $new_schema_version=2;
- 	  echo "<h2>Preparing SQL statements to update schema to version  $new_schema_version</h2>";
-	  $sql .= "UPDATE schema_info SET value='$new_schema_version' WHERE tag='schema_version';\n";
-	  $sql .= "ALTER TABLE users ADD COLUMN username text;\n";
-	  $sql .= "ALTER TABLE users ADD COLUMN account_origin text;\n";
-	  $db -> ExecSql ("SELECT user_id FROM users", $results, false);
-	  foreach ($results as $row)
-	    {
-	      $user_id=$db->EscapeString($row['user_id']);
-	      $sql .= "UPDATE users SET username='$user_id', user_id='".get_guid()."', account_origin='".LOCAL_USER_ACCOUNT_ORIGIN."' WHERE user_id='$user_id';\n";
-	    }
-	  $sql .= "CREATE UNIQUE INDEX idx_unique_username_and_account_origin ON users (username, account_origin);\n"; 
-	  $sql .= "CREATE UNIQUE INDEX idx_unique_email_and_account_origin ON users USING btree (email, account_origin);\n";
-	}
-      
-      //  $db -> ExecSqlUpdate("BEGIN;\n$sql\nROLLBACK;\n", true);
-      $db -> ExecSqlUpdate("BEGIN;\n$sql\nCOMMIT;\n", true);
-      echo "</html></head>";
-      exit();
-    }
-}
+function update_schema() {
+	global $db;
+	echo "<html><head><h1>\n";
+	echo _("Trying to update the database schema.");
+	echo "</h1>\n";
+	$db->ExecSqlUniqueRes("SELECT * FROM schema_info WHERE tag='schema_version'", $row, false);
 
+	if (empty ($row)) {
+		echo "<h1>"._("Unable to retrieve schema version.  The database schema is too old to be updated.")."</h1>";
+		exit ();
+	} else {
+		$schema_version = $row['value'];
+		$sql = '';
+		if ($schema_version < 2) {
+			$new_schema_version = 2;
+			echo "<h2>Preparing SQL statements to update schema to version  $new_schema_version</h2>";
+			$sql .= "UPDATE schema_info SET value='$new_schema_version' WHERE tag='schema_version';\n";
+			$sql .= "ALTER TABLE users ADD COLUMN username text;\n";
+			$sql .= "ALTER TABLE users ADD COLUMN account_origin text;\n";
+			$db->ExecSql("SELECT user_id FROM users", $results, false);
+			foreach ($results as $row) {
+				$user_id = $db->EscapeString($row['user_id']);
+				$sql .= "UPDATE users SET username='$user_id', user_id='".get_guid()."', account_origin='".LOCAL_USER_ACCOUNT_ORIGIN."' WHERE user_id='$user_id';\n";
+			}
+			$sql .= "CREATE UNIQUE INDEX idx_unique_username_and_account_origin ON users (username, account_origin);\n";
+			$sql .= "CREATE UNIQUE INDEX idx_unique_email_and_account_origin ON users USING btree (email, account_origin);\n";
+		} else
+			if ($schema_version < 3) {
+				$new_schema_version = 3;
+				echo "<h2>Preparing SQL statements to update schema to version  $new_schema_version</h2>";
+				$sql .= "DROP INDEX idx_unique_email_and_account_origin;\n";
+				$sql .= "ALTER TABLE users DROP CONSTRAINT check_email_not_empty;\n";
+			}
+
+		//  $db -> ExecSqlUpdate("BEGIN;\n$sql\nROLLBACK;\n", true);
+		$db->ExecSqlUpdate("BEGIN;\n$sql\nCOMMIT;\n", true);
+		echo "</html></head>";
+		exit ();
+	}
+}
 ?>
+
