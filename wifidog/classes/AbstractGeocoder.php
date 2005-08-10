ac8880db107e0fdc8da0faaa47a0941db305ffd7
François Proulx <francois.proulx@gmail.com>
2005-08-10 20:13:23 +0000

    2005-07-25 Francois Proulx <francois.proulx@gmail.com> 	* Added Geocoders 		* /classes/AbstractGeocoder.php 		* /classes/Geocoders/GeocoderCanada.php 		* /classes/Geocoders/GeocoderUsa.php 	* This will be used for enterring GIS data 	* Coming in the next few days : set hotspot gis using Google Maps + Geocoding

---
+++ wifidog/classes/AbstractGeocoder.php
@@ -0,0 +1,181 @@
+<?php
+
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+ \********************************************************************/
+/**@file AbstractGeocoder.php
+ * @author Copyright (C) 2005 François Proulx <francois.proulx@gmail.com>
+ */
+ 
+require_once BASEPATH.'include/common.php';
+require_once BASEPATH.'classes/GenericObject.php';
+
+// Implementation classes loading is done after abstract class definition
+
+abstract class AbstractGeocoder
+{
+	// Domain-related attributes
+	private $civic_number = "";
+	private $street_name = "";
+	private $city = "";
+	private $province = "";
+	private $country = "";
+	private $postal_code = "";
+	
+	// Implementation attributes
+	private $endpoint_url;
+	// This value is only used to prevent from running to same query twice
+	private $execute_query = true; 
+	
+	// Factory method hash map
+	private static $implementations_map = array("Canada" => "GeocoderCanada", "USA" => "GeocoderUsa");
+	
+	/** Returns a list of countries for which we provide a geocoder implementation 
+	 * @return array Array of string keys
+	 */
+	public static function getAvailableCountriesList()
+	{
+		return array_keys(self::$implementations_map);
+	}
+	
+	/** Returns a list of available geocoders implementations
+	 * @return array Array of string keys
+	 */
+	public static function getAvailableGeocoderImplementations()
+	{
+		return array_values(self::$implementations_map);
+	}
+	
+	/** Instanciates a new Geocoder implementation
+	 * @param String $country_key The key corresponding to an entry given by getAvailableCountriesList
+	 * @return AbstractGeocoder implementation
+	 */
+	public static function getGeocoder($country_key)
+	{
+		// Return the mapped implementation
+		if(!isset(self::$implementations_map[$country_key]))
+			return null;
+		$class_name = self::$implementations_map[$country_key];
+		return new $class_name;
+	}
+	
+	public function getCivicNumber()
+	{
+		return $this->civic_number;
+	}
+	
+	public function setCivicNumber($civic_number)
+	{
+		$this->trashResponse();
+		$this->civic_number = $civic_number;
+	}
+	
+	public function getStreetName()
+	{
+		return $this->street_name;
+	}
+	
+	public function setStreetName($street_name)
+	{
+		$this->trashResponse();
+		$this->street_name = $street_name;
+	}
+	
+	public function getCity()
+	{
+		return $this->city;
+	}
+	
+	public function setCity($city)
+	{
+		$this->trashResponse();
+		$this->city = $city;
+	}
+	
+	public function getProvince()
+	{
+		return $this->province;
+	}
+	
+	public function setProvince($province)
+	{
+		$this->trashResponse();
+		$this->province = $province;
+	}
+	
+	public function getCountry()
+	{
+		return $this->country;
+	}
+	
+	protected function setCountry($country)
+	{
+		$this->trashResponse();
+		$this->country = $country;
+	}
+	
+	public function getPostalCode()
+	{
+		return $this->postal_code;
+	}
+	
+	public function setPostalCode($postal_code)
+	{
+		$this->trashResponse();
+		$this->postal_code = $postal_code;
+	}
+	
+	public function getEndpointUrl()
+	{
+		return $this->endpoint_url;
+	}
+	
+	protected function setEndpointUrl($url)
+	{
+		$this->trashResponse();
+		$this->endpoint_url = $url;
+	}
+	
+	protected function shouldExecuteQuery()
+	{
+		return $this->execute_query;
+	}
+	
+	protected function trashResponse()
+	{
+		$this->execute_query = true;
+	}
+	
+	protected function keepResponse()
+	{
+		$this->execute_query = false;
+	}
+	
+	abstract public function validateAddress();
+	abstract public function getLatitude();
+	abstract public function getLongitude();
+	abstract public function getAltitude();
+
+} // End class
+
+$class_names = AbstractGeocoder :: getAvailableGeocoderImplementations();
+foreach ($class_names as $class_name)
+	require_once BASEPATH.'classes/Geocoders/'.$class_name.'.php';
+
+?>
---
+++ wifidog/classes/Geocoders/GeocoderCanada.php
@@ -0,0 +1,139 @@
+<?php
+
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+ \********************************************************************/
+/**@file GeocoderCanada.php
+ * @author Copyright (C) 2005 François Proulx <francois.proulx@gmail.com>
+ */
+
+require_once BASEPATH.'include/common.php';
+require_once BASEPATH.'classes/GenericObject.php';
+require_once BASEPATH.'classes/AbstractGeocoder.php';
+
+class GeocoderCanada extends AbstractGeocoder
+{
+	private $cached_latitude;
+	private $cached_longitude;
+	
+	public function __construct()
+	{
+		$this->setCountry("Canada");
+		$this->setEndpointUrl("http://geocode.ca/?");
+	}
+
+	/** Run regexp to verify the postal code
+	 * @return boolean 
+	 */
+	private function validatePostalCode()
+	{
+		// Match canadian postal code (ex. J4D 3D9)
+		return preg_match("/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i", $this->getPostalCode());
+	}
+	
+	/** Validate address, making sure we don't send an HTTP for nothing
+	 * @return boolean 
+	 */
+	public function validateAddress()
+	{
+		// Make sure a city or a postal code has been entered
+		if(($this->getCivicNumber() || $this->getStreetName() == "" || $this->getCity() == "" || $this->getProvince() == "") && !$this->validatePostalCode())
+			return false;
+		return true;
+	}
+
+	/** Constructs the HTTP query string that will be sent to the endpoint URL
+	 * @return string HTTP GET query string
+	 */
+	private function buildQuery()
+	{
+		// Build HTTP GET query string containing all parameters
+		$http_params = array ("geoit" => "XML", "addresst" => $this->getStreetName(), "stno" => $this->getCivicNumber(), "city" => $this->getCity(), "prov" => $this->getProvince(), "postal" => $this->getPostalCode());
+		return $this->getEndpointUrl().http_build_query($http_params);
+	}
+
+	/** Runs the HTTP GET query
+	 * @return boolean 
+	 */
+	private function executeQuery()
+	{
+		// Don't send multiple queries when the input has not changed
+		if($this->shouldExecuteQuery() == true)
+		{
+			// Load the XML document
+			if (($dom = DOMDocument :: load($this->buildQuery())) !== false)
+			{
+				$xpath = new DOMXpath($dom);
+				
+				// Skip if there was an error
+				if($xpath->query("/geodata/error")->length >= 1)
+					return false;
+			
+				// Run XPath quries to extract data		
+				$this->cached_latitude = $xpath->query("//geodata/latt")->item(0)->nodeValue;
+				$this->cached_longitude = $xpath->query("//geodata/longt")->item(0)->nodeValue;
+				
+				// Prevent from sending multiple queries.
+				$this->keepResponse();
+			}
+			else
+				return false;
+		}
+		return true;
+	}
+
+	/** Get the latitude for enterred infos
+	 * @return string latitude ( decimal format 6-digits precision )
+	 */
+	public function getLatitude()
+	{
+		if ($this->validateAddress())
+			if ($this->executeQuery() == true)
+				return $this->cached_latitude;
+			else
+				return null;
+		else
+			return null;
+	}
+
+	/** Get the longitude for enterred infos
+	 * @return string longitude ( decimal format 6-digits precision )
+	 */
+	public function getLongitude()
+	{
+		if ($this->validateAddress())
+			if ($this->executeQuery() == true)
+				return $this->cached_longitude;
+			else
+				return null;
+		else
+			return null;
+	}
+
+	/** Get the altitude for enterred infos
+	 * @return string algitude
+	 */
+	public function getAltitude()
+	{
+		// Not supported by geocode.ca
+		return null;
+	}
+
+} // End class
+?> 
