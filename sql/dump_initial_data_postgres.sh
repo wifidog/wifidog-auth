76bfd5f3cd2ef737dc20b82f0e960a6ec58ed00b
Benoit Grégoire <benoitg@coeus.ca>
2005-08-15 19:04:40 +0000

    2005-08-15 Benoit Gr�goire  <bock@step.polymtl.ca> 	* Run sync_sql_for_cvs.sh 	* sql/dump_initial_data_postgres.sh:  Fix a hardcoded reference to rss_url which no longer exists 	* .project:  Remove mistakenly commited file in last commit 	* .cvsignore:  Add file to avoid previous error

--- sql/dump_initial_data_postgres.sh
+++ sql/dump_initial_data_postgres.sh
@@ -6,7 +6,7 @@ pg_dump -a -D --username=wifidog -t token_status
 
 pg_dump -a -D --username=wifidog -t venue_types
 pg_dump -a -D --username=wifidog -t node_deployment_status
-echo "INSERT INTO nodes (node_id, name, rss_url) VALUES ('default', 'Unknown node', NULL);"
+echo "INSERT INTO nodes (node_id, name) VALUES ('default', 'Unknown node');"
 
 pg_dump -a -D --username=wifidog -t schema_info
 pg_dump -a -D --username=wifidog -t locales
