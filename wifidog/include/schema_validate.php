bbbc6fb2ce149fc8ceadf5d2f23f28dd8cfc68db
François Proulx <francois.proulx@gmail.com>
2005-11-13 01:03:04 +0000

    *** empty log message ***

--- wifidog/include/schema_validate.php
+++ wifidog/include/schema_validate.php
@@ -709,8 +709,9 @@ function update_schema()
 		{
 			echo "<h2>Preparing SQL statements to update schema to version  $new_schema_version</h2>\n";
 			$sql .= "\n\nUPDATE schema_info SET value='$new_schema_version' WHERE tag='schema_version';\n";
-			$sql .= "ALTER TABLE flickr_photostream ADD COLUMN photo_display_mode text;";
+			$sql .= "ALTER TABLE flickr_photostream ADD COLUMN photo_display_mode text;\n";
 			$sql .= "ALTER TABLE flickr_photostream ALTER COLUMN photo_display_mode SET DEFAULT 'PDM_GRID'::text;\n";
+			$sql .= "UPDATE flickr_photostream SET photo_display_mode = 'PDM_GRID';\n";
 			$sql .= "ALTER TABLE flickr_photostream ALTER COLUMN photo_display_mode SET NOT NULL;\n";
 		}
 		
