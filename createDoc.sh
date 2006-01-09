c7ec2c7d67ea4ce540373e66b040ac98a859d155
Max Horvath <max.horvath@maxspot.de>
2006-01-09 16:00:19 +0000

    2005-01-09 Max Horvath <max.horvath@maxspot.de> 	* createDoc.sh: added description of how to install requirements

--- createDoc.sh
+++ createDoc.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #
 # Creates the WiFiDog documentation
+# =================================
 #
-# Make sure you've installed PEAR::PhpDocumentor version 1.3+ on your computer!
+# Requirements to create the documentation:
+# - PEAR::PhpDocumentor version 1.3+ must be installed
+# - the stable version doesn't support PHP5 code
+# - install PEAR::PhpDocumentor version 1.3 this way:
+#   pear install PhpDocumentor-beta
+# - memory_limit flag in php.ini must be larger than 8 MB (at least 16 MB recommended)
 
-phpdoc -t  doc -d  wifidog/ -i  wifidog/admin/templates/,wifidog/classes/AbstractDbMySql.php,wifidog/images/,wifidog/includes/HTMLeditor/,wifidog/js/,wifidog/lib/FCKeditor/,wifidog/lib/magpie/,wifidog/lib/Phlickr/,wifidog/lib/smarty/,wifidog/local_content/,wifidog/locale/,wifidog/templates/,wifidog/tmp/,local.config.php -pp on -s on -ti "WiFiDog Documentation" -o  HTML:frames:default
+phpdoc -t  doc -d  wifidog/,doc -i  wifidog/admin/templates/,wifidog/classes/AbstractDbMySql.php,wifidog/images/,wifidog/includes/HTMLeditor/,wifidog/js/,wifidog/lib/FCKeditor/,wifidog/lib/magpie/,wifidog/lib/Phlickr/,wifidog/lib/smarty/,wifidog/local_content/,wifidog/locale/,wifidog/templates/,wifidog/tmp/,local.config.php -pp on -s on -ti "WiFiDog Documentation" -o  HTML:frames:default
