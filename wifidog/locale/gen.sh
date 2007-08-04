356d006ccc5436c61ec23c4b7c49888529180a51
Benoit Grégoire <benoitg@coeus.ca>
2007-08-04 23:09:21 +0000

    * locale/gen.sh:  Major change to .po file generation.  Now uses msgmerge so we won't completely throw away previous efforts by translators (will preserve comments, and use fuzzy translation).  It's also much faster (seconds instead of minutes), and produces less strings to translate.

--- wifidog/locale/gen.sh
+++ wifidog/locale/gen.sh
@@ -1,20 +1,22 @@
+echo "Extracting strings from Smarty templates"
+echo '<?php' > smarty.txt
+find ../templates -name "*.html" -exec ./gensmarty.pl {} >> smarty.txt \;
+find ../templates/classes -name "*.tpl" -exec ./gensmarty.pl {} >> smarty.txt \;
+find ../templates/sites -name "*.tpl" -exec ./gensmarty.pl {} >> smarty.txt \;
+find ../admin/templates -name "*.html" -exec ./gensmarty.pl {} >> smarty.txt \;
+echo '?>' >> smarty.txt
+POT_FILE="message.pot"
+	rm -f $POT_FILE
+	echo "Creating new .POT file"
+	find .. -maxdepth 1 -name "*.php" -exec xgettext --language=PHP --from-code=utf-8 -o $POT_FILE --keyword=_ {} \;
+	for dir in admin auth content cron include lib/feedpressreview login portal; do
+		find ../$dir -maxdepth 1 -name "*.php" -exec xgettext --language=PHP --from-code=utf-8 -j -o $POT_FILE --keyword=_ {} \;
+	done
+	find ../classes -maxdepth 3 -name "*.php" -exec xgettext --language=PHP --from-code=utf-8 -j -o $POT_FILE --keyword=_ {} \;
+	xgettext  --language=PHP --from-code=utf-8 -j -o $POT_FILE --keyword=_ smarty.txt
+
 for i in `find . -maxdepth 1 -mindepth 1 -type d -and -not -name ".svn"`; do
-	echo "Extracting strings from Smarty templates"
-
-    echo '<?php' > smarty.txt
-    find ../templates -name "*.html" -exec ./gensmarty.pl {} >> smarty.txt \;
-    find ../templates/classes -name "*.tpl" -exec ./gensmarty.pl {} >> smarty.txt \;
-    find ../templates/sites -name "*.tpl" -exec ./gensmarty.pl {} >> smarty.txt \;
-    find ../admin/templates -name "*.html" -exec ./gensmarty.pl {} >> smarty.txt \;
-    echo '?>' >> smarty.txt
-
-    FILE="$i/LC_MESSAGES/messages.po"
-    touch $FILE
-    echo "Beginning to update $i locale .PO file"
-    find .. -maxdepth 1 -name "*.php" -exec xgettext --language=PHP --from-code=utf-8 -j -o $FILE --keyword=_ {} \;
-    for dir in admin auth content cron include lib/feedpressreview login portal; do
-        find ../$dir -maxdepth 1 -name "*.php" -exec xgettext --language=PHP --from-code=utf-8 -j -o $FILE --keyword=_ {} \;
-    done
-    find ../classes -maxdepth 3 -name "*.php" -exec xgettext --language=PHP --from-code=utf-8 -j -o $FILE --keyword=_ {} \;
-    xgettext  --language=PHP --from-code=utf-8 -j -o $FILE --keyword=_ smarty.txt
+	FINAL_FILE="$i/LC_MESSAGES/messages.po"
+	echo "Merging with previous $i .PO file"
+	msgmerge --update $FINAL_FILE $POT_FILE 
 done
