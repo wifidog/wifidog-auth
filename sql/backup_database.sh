5b2d626662a58ca84f2cb1311cb91d2ce49cda92
Benoit Grégoire <benoitg@coeus.ca>
2007-08-28 21:51:23 +0000

    * Added vacuum_last_resort_fix_backup_restore_database.sh.  This is DANGEROUS, and should only be run when your neglected (or had problem with) your VACCUMs, 	your database has become slow as molases, and VACUUM FULL is not an option because your db has grown too big.

--- sql/backup_database.sh
+++ sql/backup_database.sh
@@ -8,4 +8,11 @@ echo "You must specify a filename as the first argument"
 exit 1
 fi
 
-pg_dump --blobs --file=$FILENAME --format=c -i -O -o -v --compress=3 -U $USERNAME $DATABASE_NAME
+$cmd="pg_dump --blobs --file=$FILENAME --format=c -i -O -o -v --compress=3 -U $USERNAME $DATABASE_NAME"
+echo $cmd
+$cmd
+retval=$?
+if [[ $retval -ne 0 ]] ; then
+    echo "Unable to dump the database"
+    exit 1
+fi
