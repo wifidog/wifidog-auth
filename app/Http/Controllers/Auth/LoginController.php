d5eea3ce801a14082c8783cdc1d844c0c2419c6e
sinkcup <sinkcup@live.it>
2017-04-27 20:44:28 +0800

    new: wifidog login is ok

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -21,13 +22,6 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
      * Create a new controller instance.
      *
      * @return void
@@ -35,5 +29,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    public function redirectTo()
+    {
+        $uri = '/home';
+        if (\Request::has('gw_address') && \Request::has('gw_port')) {
+            $uri = 'http://' . \Request::input('gw_address') . ':' . \Request::input('gw_port') . '/wifidog/auth?token=' . \Request::session()->get('_token');
+        }
+        return $uri;
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login', \Request::all());
     }
 }
