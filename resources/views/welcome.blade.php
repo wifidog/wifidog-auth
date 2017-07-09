086a892b3d525c2fe177557e57cf14c9dc02d2e0
sinkcup <sinkcup@live.it>
2017-07-09 23:31:31 +0800

    opt: remove google fonts, make page fast

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,6 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
